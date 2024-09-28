df6c3f5e19f701e005975de620de329d3f2685af
franbuon <francescogiuseppe.buono@marconibari.edu.it>
2024-09-28 09:17:14 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,36 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    float num1, num2, num3, massimo;
+    cout<<"Scrivi il primo numero"<<endl;
+    cin>>num1;
+    cout<<"Scrivi il secondo numero"<<endl;
+    cin>>num2;
+    cout<<"Scrivi il terzo numero"<<endl;
+    cin>>num3;
+    if(num1>num2)
+    {
+       if(num1>num3)
+       {
+        massimo=num1;
+       }
+       else
+       {
+        massimo=num3;
+       }
+    }
+    else
+    {
+        if(num2>num3)
+        {
+            massimo=num2;
+        }
+        else
+        {
+            massimo=num3;
+        }
+    }
+    cout<<"il numero massimo è "<<massimo;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
